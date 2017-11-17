f54273a8c05f0ae2e7ebb7231a64f9c3a25d1d86
Hendrik Vloet <vloeth@tfpool17.informatik.uni-freiburg.de>
2017-11-17 22:35:53 +0100

    finished task2, task3 still pending

--- DL_Playground/Exercise 2/ex02.py
+++ DL_Playground/Exercise 2/ex02.py
@@ -8,6 +8,8 @@ import argparse
 import sys
 import os
 import shutil
+import numpy as np
+import time
 
 import tensorflow as tf
 import matplotlib.pyplot as plt
@@ -65,82 +67,188 @@ def drop_out(input,name="dropout"):
 		h_drop_out = tf.nn.dropout(input, keep_prob)
 		return keep_prob, h_drop_out
 
+def plot_training_curve(data):
+	plt.figure()
+	for i,val in enumerate(data):
+		plt.plot(data[i],label="learning rate: {}".format(LEARNING_RATE[i]))
+	plt.title("Average Training Accuracy per Epoch")
+	plt.xlabel("Epoch")
+	plt.ylabel("Accuracy")
+	plt.legend()
+	return
+
+def plot_testing_curve(data):
+	plt.figure()
+	for i,val in enumerate(data):
+		plt.plot(data[i],label="learning rate: {}".format(LEARNING_RATE[i]))
+	plt.title("Testing Accuracy per Epoch")
+	plt.xlabel("Epoch")
+	plt.ylabel("Accuracy")
+	plt.legend()
+
+def plot_cost_curve(data):
+	plt.figure()
+	for i,val in enumerate(list_cost):
+		plt.plot(data[i],label="learning rate: {}".format(LEARNING_RATE[i]))
+	plt.title("Average Cost per Epoch")
+	plt.xlabel("Epoch")
+	plt.ylabel("Cost")
+	plt.legend()
 
 
 ### MAIN ####
-EPOCHS = 10
-learning_rate = [0.1,0.01,0.001,0.0001]
-logs_path = "/tmp/mnist/1"
-'''
-tensorboard --logdir=run1:/tmp/mnist/1,run2:/tmp/mnist/1 --port=6006
-'''
+CHECK_CPU = 0
+EPOCHS = 1
+BATCH_SIZE = 55000
+N_MINIBATCH_UPDATES = int(55000/BATCH_SIZE)
+LEARNING_RATE = [0.1,0.01,0.001,0.0001]
+
+# filter sizes for experimentation 
+# GPU {8, 16, 32, 64, 128, 256}
+# CPU {8, 16, 32, 64}
+if CHECK_CPU:
+	FILTER_SIZE = [8, 16, 32, 64]
 
 
-list_cost = []
-list_train_acc = []
+if CHECK_CPU==0:
+	FILTER_SIZE = [8, 16, 32, 64, 128, 256]
 
+INPUT_DEPTH = 1
+FILTER_WIDTH = 3
+PARAMETER_NUM = (FILTER_WIDTH*FILTER_WIDTH*INPUT_DEPTH+1)*FILTER_SIZE
 
 # setup data 55000 training samples, 10000 test samples
-
+print("loading data...")
 mnist = input_data.read_data_sets("MNIST_data/", one_hot = True)
+print("loading finished!")
 
-x  = tf.placeholder(tf.float32,([None,784]),name="x")
-y_ = tf.placeholder(tf.float32,([None,10]),name="labels")
-
-# reshaping
-x_image = tf.reshape(x,[-1,28,28,1])
-
-# Layer 1: convolution
-h_conv1 = conv_layer(x_image,[3,3,1,16],"conv_1")
-h_pool1 = max_pool_2x2(h_conv1)
-
-# Layer 2: convolution
-h_conv2 = conv_layer(h_pool1,[3,3,16,16],"conv_2")
-h_pool2 = max_pool_2x2(h_conv2)
+for index,filter_depth in enumerate(FILTER_SIZE):
+	list_cost = [[] for i in range(len(LEARNING_RATE))]
+	list_train_acc = [[] for i in range(len(LEARNING_RATE))]
+	list_test_acc = [[] for i in range(len(LEARNING_RATE))]
 
 
-# Layer 3: fully connected
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*16])
-h_fc1 = fc_layer(h_pool2_flat, [7*7*16,128],"fc_1")
-
-# Dropout Regularization
-keep_prob, h_fc1_drop = drop_out(h_fc1)
-
-# Layer 4: Output with Softmax and Cross Entropy Loss
-y_conv  = softmax_output(h_fc1_drop,[128,10],name="softmax")
-cross_entropy = xent(y_conv)
-
-
-train_step = tf.train.GradientDescentOptimizer(0.1).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y_conv,1),tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-training_summary = tf.summary.scalar("training_accuracy",accuracy)
-testing_summary = tf.summary.scalar("testing_summary",accuracy)
-
-# create summaries for cost and accuracy
-xent_summary = tf.summary.scalar("X-Entropy", cross_entropy)
-
-# merge all summaries into a single operation
-#summary_op = tf.summary.merge_all()
-
-with tf.Session() as sess:
-	# initialize global variables
-	sess.run(tf.global_variables_initializer())
-	# create log writer object
-	writer = tf.summary.FileWriter(logs_path, graph=tf.get_default_graph())
-        
 	
-        
 
-	# epoch loop
-	for ep in range(EPOCHS):
-		print("Epoch: {}".format(ep))
-		# batch loop, 55000 samples, batchsize is 50 -> 1100 training loops per epoch
-		for i in range(1100):
-			batch_x, batch_y = mnist.train.next_batch(50)           
-			train_step.run(feed_dict={x: batch_x, y_: batch_y, keep_prob: 0.5})
-		
+	x  = tf.placeholder(tf.float32,([None,784]),name="x")
+	y_ = tf.placeholder(tf.float32,([None,10]),name="labels")
+
+	# reshaping
+	x_image = tf.reshape(x,[-1,28,28,1])
+
+	# Layer 1: convolution
+	h_conv1 = conv_layer(x_image,[3,3,1,filter_depth],"conv_1")
+	h_pool1 = max_pool_2x2(h_conv1)
+
+	# Layer 2: convolution
+	h_conv2 = conv_layer(h_pool1,[3,3,filter_depth,filter_depth],"conv_2")
+	h_pool2 = max_pool_2x2(h_conv2)
+
+
+	# Layer 3: fully connected
+	h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*filter_depth])
+	h_fc1 = fc_layer(h_pool2_flat, [7*7*filter_depth,128],"fc_1")
+
+	# Dropout Regularization
+	keep_prob, h_fc1_drop = drop_out(h_fc1)
+
+	# Layer 4: Output with Softmax and Cross Entropy Loss
+	y_conv  = softmax_output(h_fc1_drop,[128,10],name="softmax")
+	cross_entropy = xent(y_conv)
+
+	correct_prediction = tf.equal(tf.argmax(y_conv,1),tf.argmax(y_,1))
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+	print("starting training now...")
+	print("Now using {} filters:".format(filter_depth))
+	start_time = time.time()
+	for idx,val in enumerate(LEARNING_RATE):
+		# adjust learning rate
+		train_step = tf.train.GradientDescentOptimizer(LEARNING_RATE[idx]).minimize(cross_entropy)
+		# open new session for every learning rate
+		with tf.Session() as sess:
+			print("---------------------------------------------------------------")
+			
+			# initialize global variables
+			sess.run(tf.global_variables_initializer())
+			# learning rate loop
+			
+			# epoch loop
+			for ep in range(EPOCHS):
+				train_acc = 0;
+				loss = 0;
+				print("Epoch: {}\t Learning Rate: {}".format(ep,val))
+				# batch loop, 55000 samples, batchsize is 50 -> 1100 training loops per epoch
+				for i in range(N_MINIBATCH_UPDATES):
+					batch_x, batch_y = mnist.train.next_batch(BATCH_SIZE)           
+					train_step.run(feed_dict={x: batch_x, y_: batch_y, keep_prob: 0.5})
+					# accumulated training accuracy
+					train_acc += sess.run(accuracy,feed_dict={x:batch_x, y_:batch_y, keep_prob:0.5})
+					# accumulated cross entropy loss
+					loss +=sess.run(cross_entropy, feed_dict={x:batch_x,y_: batch_y, keep_prob:0.5})
+							
+				print("Average Training Accurancy in current epoch: \t\t{:.4f}".format(
+					train_acc / N_MINIBATCH_UPDATES))
+				list_train_acc[idx].append(train_acc/N_MINIBATCH_UPDATES)
+				
+					
+				print("Average Cost in current epoch: \t\t\t\t{:.4f}".format(loss/N_MINIBATCH_UPDATES))
+				list_cost[idx].append(loss/N_MINIBATCH_UPDATES)
+
+
+				#test_acc = accuracy.eval(feed_dict={x:mnist.test.images,y_:mnist.test.labels,keep_prob: 1}))
+				test_acc = sess.run(accuracy, feed_dict={x:mnist.test.images,
+												y_:mnist.test.labels,keep_prob: 1})
+				print("Test Accuracy in current epoch: \t\t\t{:.4f}".format(test_acc))
+				list_test_acc[idx].append(test_acc)
+				print("---------------------------------------------------------------")
+		end_time = time.time()
+		time_needed = end_time - start_time
+	print("Done after {:.4f} min".format(time_needed/60))
+
+	# plotting stuff
+
+
+
+
+
+	plot_training_curve(list_train_acc)
+
+	plot_testing_curve(list_test_acc)
+
+	plot_cost_curve(list_cost)
+
+
+
+	#plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####################################
+### CODE DUMPSTER - IGNORE BELOW ###
+####################################
+
+
+
+
+
+'''
 			if i%50==0:
 				train_acc, train_summ = sess.run([accuracy, training_summary],
 										feed_dict={x:batch_x, y_:batch_y, keep_prob:0.5})
@@ -151,37 +259,13 @@ with tf.Session() as sess:
 																			y_: batch_y, keep_prob:0.5})
 				writer.add_summary(cost_summ,i)
 				list_cost.append(xent)
-			
+			'''
 				
 		
-			if i%50 == 0:
-				print('step {}, training accuracy {:.4f}, cost {:.4f}'.format(i, train_acc, xent))
+			#if i%50 == 0:
+			#	print('step {}, training accuracy {:.4f}, cost {:.4f}'.format(i, train_acc, xent))
 				
 			#if epoch % 5 == 0:
-				
-		
-		test_acc, test_summ = sess.run([accuracy, testing_summary],feed_dict={
-					x:mnist.test.images,y_:mnist.test.labels,keep_prob: 1})
-		writer.add_summary(test_summ,i)
-		print("Accuracy in epoch {}: {}".format(ep,accuracy.eval(feed_dict={
-				x:mnist.test.images,y_:mnist.test.labels,keep_prob: 1})))		
-	print("done")
-
-plt.figure()
-plt.plot(list_train_acc)
-plt.show()
-		
-		
-
-
-
-
-
-
-
-
-
-		
 		
 		
 		
@@ -225,16 +309,6 @@ if ADAM:
 		print('test accuracy %g' % accuracy.eval(feed_dict={x: mnist.test.images, y_:
 														  mnist.test.labels, keep_prob: 1.0}))
 
-
-
-
-
-
-
-
-
-
-
 '''
 
 
@@ -245,20 +319,6 @@ if ADAM:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-####################################
-### CODE DUMPSTER - IGNORE BELOW ###
-####################################
 '''
 
 
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Deep-Learning-Lab-Course
 
-__DL_Playground__: working folder for exercises
+__DL_Playground__: working folder for exercises, ignore (@ tutors)
 
 __dl_lab_source__: source code from the tutors
 
 
 
-__Exercise 1__: Neural Network from Scratch with Python2
+__Exercise 1__: Neural Network from Scratch with Python2, SUBMISSION FOLDER
 
-__Exercise 2__: Introduction into Tensorflow
+__Exercise 2__: Introduction into Tensorflow, SUBMISSION FOLDER
