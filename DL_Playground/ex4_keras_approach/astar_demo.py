823ecf463011f70b9abe6f87b26caa63f91a3748
Hendrik <hendrik.vloet@gmx.de>
2018-01-21 00:52:38 +0100

    added astar simulator

--- DL_Playground/ex4_keras_approach/astar_demo.py
+++ DL_Playground/ex4_keras_approach/astar_demo.py
@@ -5,16 +5,14 @@ import matplotlib.pyplot as plt
 from random import randrange
 # custom modules
 from utils     import Options
-from simulator import Simulator
+from astar_simulator import Simulator
 
 class astar_solver():
-    __init__(self):
-        _execute_demo()
-        
-        
+    def __init__(self,num_episodes):
+        self.number_of_demo_episodes = num_episodes
+        self.needed_steps = []
+        self._execute_demo()        
     def _execute_demo(self):
-                
-            
         # 0. initialization
         opt = Options()
         sim = Simulator(opt.map_ind, opt.cub_siz, opt.pob_siz, opt.act_num)
@@ -26,33 +24,35 @@ class astar_solver():
         epi_step = 0    # #steps in current episode
         nepisodes = 0   # total #episodes executed
 
-
-        for step in range(100):
-            if epi_step == 0:
-                state = sim.newGame(opt.tgt_y, opt.tgt_x)
-                nepisodes += 1
-            else:
-                # will perform A* actions
-                # this is the part where your agent later
-                # would take its action
-                state = sim.step()
-                # instead you could also take a random action
-                # state = sim.step(randrange(opt.act_num))
-
-            epi_step += 1
-
-            if state.terminal or epi_step >= opt.early_stop:
-                epi_step = 0
-
-            if opt.disp_on:
-                if win_all is None:
-                    plt.subplot(121)
-                    win_all = plt.imshow(state.screen)
-                    plt.subplot(122)
-                    win_pob = plt.imshow(state.pob)
+        for ep in range(self.number_of_demo_episodes):
+            for step in range(100):
+                if epi_step == 0:
+                    state = sim.newGame(opt.tgt_y, opt.tgt_x)
+                    nepisodes += 1
                 else:
-                    win_all.set_data(state.screen)
-                    win_pob.set_data(state.pob)
-                plt.pause(opt.disp_interval)
-                plt.draw()
+                    # will perform A* actions
+                    # this is the part where your agent later
+                    # would take its action
+                    state = sim.step(None)
 
+                    # instead you could also take a random action
+                    # state = sim.step(randrange(opt.act_num))
+
+                epi_step += 1
+
+                if state.terminal or epi_step >= opt.early_stop:
+                    # astar always succeeds
+                    self.needed_steps.append(epi_step)
+                    epi_step = 0
+
+                if opt.disp_on:
+                    if win_all is None:
+                        plt.subplot(121)
+                        win_all = plt.imshow(state.screen)
+                        plt.subplot(122)
+                        win_pob = plt.imshow(state.pob)
+                    else:
+                        win_all.set_data(state.screen)
+                        win_pob.set_data(state.pob)
+                    plt.pause(opt.disp_interval)
+                    plt.draw()
--- DL_Playground/ex4_keras_approach/evaluate_agent.py
+++ DL_Playground/ex4_keras_approach/evaluate_agent.py
@@ -7,21 +7,22 @@ from random import randrange
 import tensorflow as tf
 import keras
 from collections import deque
-from keras.models import Sequential
-from keras.optimizers import Adam
-from keras import backend as K
-from keras.layers import Conv2D, Flatten, Dense, Dropout
+# from keras.models import Sequential
+# from keras.optimizers import Adam
+# from keras import backend as K
+# from keras.layers import Conv2D, Flatten, Dense, Dropout
 
 # custom modules
 from utils     import Options, rgb2gray
+from astar_simulator import Simulator
 from simulator import Simulator
 from transitionTable import TransitionTable
 import random
 
 
-from train_agent import Agent
+# from train_agent import Agent
 from astar_demo import astar_solver
 import sys
 sys.path.append('..')
 
-astar_agent = astar_solver()
+astar_agent = astar_solver(10)
