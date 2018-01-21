1c4bae52112fd864d9138917b94fa1ed8e143032
Hendrik Vloet <vloeth@tfpool63.informatik.uni-freiburg.de>
2018-01-21 15:57:07 +0100

    added model saving and loading routing, added testing area

--- DL_Playground/ex4_keras_approach/astar_demo.py
+++ DL_Playground/ex4_keras_approach/astar_demo.py
@@ -5,54 +5,62 @@ import matplotlib.pyplot as plt
 from random import randrange
 # custom modules
 from utils     import Options
-from simulator import Simulator
+from astar_simulator import Simulator
 
 class astar_solver():
-    __init__(self):
-        _execute_demo()
+    def __init__(self,episodes):
+        self.nepisodes = episodes
+        self.list_epi_steps = []
         
+        self._execute_demo()
         
     def _execute_demo(self):
                 
             
         # 0. initialization
         opt = Options()
-        sim = Simulator(opt.map_ind, opt.cub_siz, opt.pob_siz, opt.act_num)
-
+        self.sim = Simulator(opt.map_ind, opt.cub_siz, opt.pob_siz, opt.act_num)
+        terminal = False
         # 1. demo control loop
+        opt.disp_on = False
         if opt.disp_on:
             win_all = None
             win_pob = None
         epi_step = 0    # #steps in current episode
         nepisodes = 0   # total #episodes executed
 
-
-        for step in range(100):
-            if epi_step == 0:
-                state = sim.newGame(opt.tgt_y, opt.tgt_x)
-                nepisodes += 1
-            else:
-                # will perform A* actions
-                # this is the part where your agent later
-                # would take its action
-                state = sim.step()
-                # instead you could also take a random action
-                # state = sim.step(randrange(opt.act_num))
-
-            epi_step += 1
-
-            if state.terminal or epi_step >= opt.early_stop:
-                epi_step = 0
-
-            if opt.disp_on:
-                if win_all is None:
-                    plt.subplot(121)
-                    win_all = plt.imshow(state.screen)
-                    plt.subplot(122)
-                    win_pob = plt.imshow(state.pob)
+        for i in range(self.nepisodes):
+            terminal = False
+            while not terminal:
+                if epi_step == 0:
+                    state = self.sim.newGame(opt.tgt_y, opt.tgt_x)
+                    nepisodes += 1
                 else:
-                    win_all.set_data(state.screen)
-                    win_pob.set_data(state.pob)
-                plt.pause(opt.disp_interval)
-                plt.draw()
+                    # will perform A* actions
+                    # this is the part where your agent later
+                    # would take its action
+                    state = self.sim.step(None)
+                    # instead you could also take a random action
+                    # state = sim.step(randrange(opt.act_num))
 
+                epi_step += 1
+
+                if state.terminal or epi_step >= opt.early_stop:
+                    # astar always succeeds optimal
+                    self.list_epi_steps.append(epi_step)
+                    epi_step = 0
+                
+                terminal = True if state.terminal else False
+
+                if opt.disp_on:
+                    if win_all is None:
+                        plt.subplot(121)
+                        win_all = plt.imshow(state.screen)
+                        plt.subplot(122)
+                        win_pob = plt.imshow(state.pob)
+                    else:
+                        win_all.set_data(state.screen)
+                        win_pob.set_data(state.pob)
+                    plt.pause(opt.disp_interval)
+                    plt.draw()
+        
--- DL_Playground/ex4_keras_approach/train_agent.py
+++ DL_Playground/ex4_keras_approach/train_agent.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
@@ -16,6 +17,11 @@ from keras.layers import Conv2D, Flatten, Dense, Dropout
 from utils     import Options, rgb2gray
 from simulator import Simulator
 from transitionTable import TransitionTable
+from astar_demo import astar_solver
+import argparse
+import sys
+#sys.path.append('..')
+parser = argparse.ArgumentParser()
 import random
 
 
@@ -26,10 +32,11 @@ class Agent:
         self.action_size = opt.act_num
         self.gamma = 0.95
         self.learning_rate = 1e-5
-        self.init_epsilon = 0.9
+        self.init_epsilon = 1.0
         self.final_epsilon = 0.1
         self.epsilon = self.init_epsilon
-        self.explore = 500000       
+        self.eps_decay_rate
+        self.explore = 100000       
         self.model = self._build_model()
         self.target_model = self._build_model()
         self.update_target_model()
@@ -95,16 +102,17 @@ class Agent:
                 
             # decay the epsilon as long it's larger then final epsilon threshold
             if(self.epsilon>self.final_epsilon):
-                self.epsilon -= ((self.init_epsilon-self.final_epsilon)/self.explore)
+                #self.epsilon -= ((self.init_epsilon-self.final_epsilon)/self.explore)
+                self.epsilon *= self.eps_decay_rate
 
     def load(self, file_name):
         self.model.load_weights(file_name)
-        print("agent loaded weights"+ file_name)
+        print("agent loaded weights from file '{}' ".format(file_name))
         self.update_target_model()
         
     def save(self, file_name):
         self.model.save_weights(file_name)
-        print("agent saved weights"+ file_name)
+        print("agent saved weights in file '{}' ".format(file_name))
 
 
 
@@ -167,14 +175,14 @@ agent.model.summary()
 
 
 #number of total trainign game episodes
-N_EPISODES_TOTAL_TRAIN = 1000 
+N_EPISODES_TOTAL_TRAIN = 1000
 SAVE_AFTER_N_EPISODES = 50
 
 # show a full episode every n episodes for opt.disp_on is true
 DISP_PROGRESS_AFTER_N_EPISODES = 5 
 
 # full random episodes before training
-FULL_RANDOM_EPISODES = 50
+FULL_RANDOM_EPISODES = 100
 
 
 
@@ -193,7 +201,8 @@ epi_step = 0
 nepisodes = 0
 display_progress = False
 #sum of a all rewards in one episode
-episode_reward = 0 
+episode_reward = 0
+list_episode_reward = []
 
 
 state = sim.newGame(opt.tgt_y, opt.tgt_x)
@@ -208,16 +217,20 @@ while nepisodes < N_EPISODES_TOTAL_TRAIN:
         
         if nepisodes % SAVE_AFTER_N_EPISODES == 0 and nepisodes != 0:
             print("saved")
-            agent.save("save/" + opt.weights_fil)
+            agent.save(opt.weights_fil)
                        
-        #print("played {}/{} episodes, episode_reward: {:.2}, epi_step {}, epsilon: {:.2}".format(nepisodes,N_EPISODES_TOTAL_TRAIN, episode_reward,epi_step, agent.epsilon))#
         
         if nepisodes%1==0:
-            print("Episode {}/{}\t|Epsilon: {:.2f}\t|Epsilon greedy ratio: {:.2f}".format(nepisodes,N_EPISODES_TOTAL_TRAIN,agent.epsilon,agent.greedy/agent.rnd))
+            print("Episode {}/{}\t|Epsilon: {:.2f}\t|e-greedy ratio: {:.2f}\t episode reward: {}".format(nepisodes+1,N_EPISODES_TOTAL_TRAIN,agent.epsilon,agent.greedy/agent.rnd,episode_reward))
 
         epi_step_hist.append(epi_step)
+        list_episode_reward.append(episode_reward) 
         episode_reward = 0
         epi_step = 0
+        
+        # update target model
+        agent.update_target_model()
+        
         nepisodes += 1
         # reset the game
         state = sim.newGame(opt.tgt_y, opt.tgt_x)
@@ -260,22 +273,124 @@ while nepisodes < N_EPISODES_TOTAL_TRAIN:
         plt.pause(opt.disp_interval)
         plt.draw()
 
+
+
+#################### TESTING AREA ############################
+
+## LOAD MODEL ################################################
+
+# creat a test agent
+test_agent = Agent()
+parser.add_argument("-w", "--weights", help=".h5 weights_file_name for conv network",
+                    default=opt.weights_fil)
+args = parser.parse_args()
+weights_file_name = args.weights
+test_agent.load(weights_file_name)
+
+
+## INITIALIZATION ############################################
+
+# history of total episode rewards
+episode_reward_hist = []
+#history of total episode step needed to solve or ealy step
+epi_step_hist = [] 
+epi_step = 0
+nepisodes = 0
+#sum of a all rewards in one episode
+episode_reward = 0 
+disp_progress = False
+
+
+N_EPISODES_TOTAL_TEST = 10#00
+DISP_PROGRESS_AFTER_N_EPISODES = 1 
+
+
+###############################################################
+
+# create astar solver and execute n times initally to generate metric to compare agent
+astar = astar_solver(N_EPISODES_TOTAL_TEST)
+agent
+print("A-Star: {}/{} solved\t| average steps needed: {:.2f}".format(astar.sim.success,N_EPISODES_TOTAL_TEST,np.mean(astar.list_epi_steps)))
+
+nepisodes_solved_cnt = 0
+termin_state = []
+
+
+state_with_history = np.zeros((opt.hist_len, opt.state_siz))
+append_to_hist(state_with_history, rgb2gray(state.pob).reshape(opt.state_siz))
+next_state_with_history = np.copy(state_with_history)
+
+
+
+
+while nepisodes < N_EPISODES_TOTAL_TEST:
+    if state.terminal or epi_step >= opt.early_stop or episode_reward < -10 :
+        disp_progress = True if nepisodes % DISP_PROGRESS_AFTER_N_EPISODES == 0 else False
+        nepisodes += 1
+        if state.terminal:
+            nepisodes_solved_cnt +=1
+            print("nepisodes_solved: {}".format(nepisodes_solved_cnt))
+
+        print("played {}/{} episodes, episode_reward: {:.2}, epi_step {}"
+                      .format(nepisodes,N_EPISODES_TOTAL_TEST, episode_reward,epi_step ))
+        termin_state.append(state.terminal)
+        epi_step_hist.append(epi_step)
+        episode_reward_hist.append(episode_reward)
+        episode_reward = 0
+        epi_step = 0
+        #astar_steps,state =start_new_game()
+        #astar_steps_hist.append(astar_steps)
+
+        # and reset the history
+        state_with_history[:] = 0
+        append_to_hist(state_with_history, rgb2gray(state.pob).reshape(opt.state_siz))
+        next_state_with_history = np.copy(state_with_history)
+
     
-        
+    action = test_agent.act(np.array([state_with_history]),False)
     
-f, axarr = plt.subplots(2,1)
+    epi_step +=1
+    next_state = sim.step(action)
+    # append to history
+    append_to_hist(next_state_with_history, rgb2gray(next_state.pob).reshape(opt.state_siz))
 
-axarr[0].plot(episode_reward_hist)
-axarr[0].set_ylabel(r'Total Reward',usetex=True)
+    # mark next state as current state
+    state_with_history = np.copy(next_state_with_history)
+    episode_reward += next_state.reward
+    state = next_state
 
-axarr[1].plot(epi_step_hist)
-axarr[1].set_ylabel(r'Number of steps',usetex=True)
+    if opt.disp_on and disp_progress:
 
-axarr[0].set_xlabel(r'Episode',usetex=True)
-axarr[1].set_xlabel(r'Episode',usetex=True)    
-    #state_batch, action_batch, next_state_batch, reward_batch, terminal_batch = trans.sample_minibatch()
+        if win_all is None:
+            plt.subplot(121)
+            win_all = plt.imshow(state.screen)
+            plt.subplot(122)
+            win_pob = plt.imshow(state.pob)
+        else:
+            win_all.set_data(state.screen)
+            win_pob.set_data(state.pob)
+        plt.pause(opt.disp_interval)
+        plt.draw()
 
-helper_save(None)
 
-# 2. perform a final test of your model and save it
-# TODO
+epi_step_hist =np.array(epi_step_hist)
+
+delta_astar = np.mean(astar.list_epi_steps) - np.mean(epi_step_hist)
+print("==============================")
+print("success rate of the agent: {}".format(nepisodes_solved_cnt/N_EPISODES_TOTAL_TEST))
+print("mean diff to astare if successful {}".format(delta_astar))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
